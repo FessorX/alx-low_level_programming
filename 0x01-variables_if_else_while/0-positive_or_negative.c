40ec8c2dcad2be3dc70a552f2d13b9bd295a3fcd
Musa Abraham <profaimusa@gmail.com>
2023-02-17 17:14:24 -0800

    first attempt

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		printf("%i is positive\n", n);
 	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
 	else if (n < 0)
 	{
 		printf("%i is negative\n", n);
+	}
+	else
+	{
+		printf("%i is zero\n", n);
 	}
 	return (0);
 }
