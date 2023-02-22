ca7f3a3be6432a40fbcfd958225506dd1cb7cfc1
Musa Abraham <profaimusa@gmail.com>
2023-02-21 19:02:18 -0800

    m

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -23,4 +23,17 @@ int print_sign(int n);
 
 int _abs(int);
 
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
 #endif
