25aff35aca7cbc6674e1996762e85f34f470cdb7
Musa Abraham <profaimusa@gmail.com>
2023-02-21 17:22:47 -0800

    first

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar- writes the character ch to stdout
+ * @ch: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char)
