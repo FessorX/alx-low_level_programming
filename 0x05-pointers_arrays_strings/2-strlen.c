df90c2b97b7069614b5fe2daa664a21a632c5cc8
Musa Abraham <profaimusa@gmail.com>
2023-02-27 09:30:52 -0800

    ...

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen- gets the length of a string
+ *
+ * @s: pointer to string
+ * return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while ((int) *s[n] != 0)
+	{
+		n++;
+	}
+	return n
+}
