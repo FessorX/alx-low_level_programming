5b146c6a2c3a3015464a8f825cb495682fe533f4
Musa Abraham <profaimusa@gmail.com>
2023-02-27 09:08:44 -0800

    first draft

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98- takes input and stores it as 98
+ *
+ * @n: pointer tpo change its value
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int- swaps value of two integers
+ *
+ * @a: pointer to 1st int
+ * @b: pointer to 2nd int
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c;
+	int d;
+
+	c = *a;
+	d = *b;
+	*a = d;
+	*b = c;
+
+
+
+}
