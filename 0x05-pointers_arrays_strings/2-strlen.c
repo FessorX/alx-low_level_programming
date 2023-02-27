92617829fa4c178d7390655d0abf696def4fb90d
Musa Abraham <profaimusa@gmail.com>
2023-02-27 14:34:50 -0800

    edited 4

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -13,7 +13,7 @@ int _strlen(char *s)
 
 	n = 0;
 
-	while (s != '\0')
+	while (*s != '\0')
 	{
 		s++;
 		n++;
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,23 +8,13 @@
 void print_rev(char *s)
 {
 
-	int len = 0;
-	int i;
+	int len;
 
-	/* iterate to find length of string and point to last character */
-	while (*s != '\0')
+	len = _strlen(s) - 1;
+
+	while (len >= 0)
 	{
-		len++;
-		++s;
-	}
-
-	/* go back to character before null character */
-		s--;
-
-	/* print string reversed */
-	for (i = len; i > 0; i--)
-	{
-		_putchar(*s);
+		_putchar(s[len]);
 		s--;
 	}
 
