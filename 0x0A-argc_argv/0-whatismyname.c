e847dd2677f68403fbffc158c6516b7399303374
Musa Abraham <profaimusa@gmail.com>
2023-03-10 17:31:54 -0800

    more

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- Prints prog name
+ * @argc: num of arguments
+ * @argv: vector of pointers
+ *
+ * Return: 0 success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Prints number and newline
+ * @argc: argument count
+ * @argv: array of pointer
+ *
+ * Return: 0 on successs
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
